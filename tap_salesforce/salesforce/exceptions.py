be54f775dad4c39a395baeb0773728d1572b7096
Kyle Allan <kyle@stitchdata.com>
2017-11-02 10:47:58 -0400

    pylint exceptions

--- tap_salesforce/salesforce/exceptions.py
+++ tap_salesforce/salesforce/exceptions.py
@@ -1,12 +1,17 @@
+# pylint: disable=super-init-not-called
+
 class TapSalesforceException(Exception):
     pass
 
+
 class TapSalesforceQuotaExceededException(TapSalesforceException):
     pass
+
 
 class TapSalesforceHTTPException(TapSalesforceException):
     def __init__(self, requestException):
         self.requestException = requestException
 
     def __str__(self):
-        return str(self.requestException) + ", Response from Salesforce: {}".format(self.requestException.response.text)
+        return str(self.requestException) + \
+            ", Response from Salesforce: {}".format(self.requestException.response.text)
