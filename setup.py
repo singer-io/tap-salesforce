f98944533d996509cbc23fce613808738da60fb5
Kyle Allan <kyle@stitchdata.com>
2017-11-15 09:54:38 -0500

    bump to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-salesforce',
-      version='0.0.8',
+      version='1.0.0',
       description='Singer.io tap for extracting data from the Salesforce API',
       author='Stitch',
       url='https://singer.io',
