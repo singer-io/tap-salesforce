d981bba2c5b324092f3d4c598163572c2778c191
Alex Zhao <azhao@klick.com>
2019-08-14 18:23:03 -0400

    Add SOAP login support

---
+++ tap_salesforce/salesforce/utils.py
@@ -0,0 +1,37 @@
+import xml.dom.minidom
+import os
+# Copied from https://github.com/simple-salesforce/simple-salesforce/blob/master/simple_salesforce/util.py
+# pylint: disable=invalid-name
+def getUniqueElementValueFromXmlString(xmlString, elementName):
+    """
+    Extracts an element value from an XML string.
+
+    For example, invoking
+    getUniqueElementValueFromXmlString(
+        '<?xml version="1.0" encoding="UTF-8"?><foo>bar</foo>', 'foo')
+    should return the value 'bar'.
+    """
+    xmlStringAsDom = xml.dom.minidom.parseString(xmlString)
+    elementsByName = xmlStringAsDom.getElementsByTagName(elementName)
+    elementValue = None
+    if len(elementsByName) > 0:
+        elementValue = elementsByName[0].toxml().replace(
+            '<' + elementName + '>', '').replace('</' + elementName + '>', '')
+    return elementValue
+
+# pylint: disable=invalid-name
+def getUniqueElementValueFromXmlElement(xmlStringAsDom, elementName):
+    """
+    Extracts an element value from an XML string.
+
+    For example, invoking
+    getUniqueElementValueFromXmlString(
+        '<?xml version="1.0" encoding="UTF-8"?><foo>bar</foo>', 'foo')
+    should return the value 'bar'.
+    """
+    elementsByName = xmlStringAsDom.getElementsByTagName(elementName)
+    elementValue = None
+    if len(elementsByName) > 0:
+        elementValue = elementsByName[0].toxml().replace(
+            '<' + elementName + '>', '').replace('</' + elementName + '>', '')
+    return elementValue
