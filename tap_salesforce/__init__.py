f623963883ab2cdf7d51701c696f6b3b9df85a4b
JingLin0 <jinglin.daisy@gmail.com>
2020-04-01 16:35:47 +0200

    turns out we need them

--- tap_salesforce/__init__.py
+++ tap_salesforce/__init__.py
@@ -132,8 +132,12 @@ def do_discover(sf):
     objects_to_discover = [
         "Account",
         "Contact",
+        "Lead",
         "Opportunity",
         "Campaign",
+        "AccountContactRelation",	
+        "AccountContactRole",	
+        "OpportunityContactRole",
     ]
     key_properties = ["Id"]
 
