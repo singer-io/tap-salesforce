c72b7c8a2620a4b8d473735be0463f65dc3c3189
cosimon <cosimon@users.noreply.github.com>
2020-04-22 15:37:29 -0400

    Bump to v1.4.32 (#81)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-salesforce',
-      version='1.4.31',
+      version='1.4.32',
       description='Singer.io tap for extracting data from the Salesforce API',
       author='Stitch',
       url='https://singer.io',
