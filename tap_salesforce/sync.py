1e6b8aab2ec0b64136969fdd5194846996f7e062
Jacob Baca <jbaca@stitchdata.com>
2020-08-25 16:24:18 +0000

    fixed pylint

--- tap_salesforce/sync.py
+++ tap_salesforce/sync.py
@@ -102,7 +102,7 @@ def sync_stream(sf, catalog_entry, state):
         try:
             sync_records(sf, catalog_entry, state, counter)
             singer.write_state(state)
-        except RequestException as ex:
+        except RequestException as ex: # pylint: disable=raise-missing-from
             raise Exception("Error syncing {}: {} Response: {}".format(
                 stream, ex, ex.response.text))
         except Exception as ex:
