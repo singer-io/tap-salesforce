58ccbdd547f3804759108594cb0a193434b05d2e
Jacob Baca <52418765+jacobrobertbaca@users.noreply.github.com>
2020-08-26 11:40:58 -0400

    bumped patch version to 1.4.34 (#87)
    * bumped minor version to 1.5.0
    
    * changed from minor version bump to patch bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-salesforce',
-      version='1.4.33',
+      version='1.4.34',
       description='Singer.io tap for extracting data from the Salesforce API',
       author='Stitch',
       url='https://singer.io',
