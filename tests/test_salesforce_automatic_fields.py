e7c39417363f0971d9d9d3180eab4c1de56eb064
Kyle Speer <54034650+kspeer825@users.noreply.github.com>
2021-02-25 13:46:07 -0500

    automatic fields test (#95)
    * automatic fields test
    
    * pylint cleanup
    
    Co-authored-by: Kyle Speer <kspeer@stitchdata.com>

---
+++ tests/test_salesforce_automatic_fields.py
@@ -0,0 +1,88 @@
+"""
+Test that with no fields selected for a stream automatic fields are still replicated
+"""
+from datetime import datetime, timedelta
+
+from tap_tester import runner, connections
+
+from base import SalesforceBaseTest
+
+
+class SalesforceAutomaticFields(SalesforceBaseTest):
+    """Test that with no fields selected for a stream automatic fields are still replicated"""
+
+    @staticmethod
+    def name():
+        return "tap_tester_salesforce_automatic_fields"
+
+    @staticmethod
+    def get_properties():  # pylint: disable=arguments-differ
+        return {
+            'start_date' : (datetime.now() + timedelta(days=-1)).strftime("%Y-%m-%dT00:00:00Z"),
+            'instance_url': 'https://cs95.salesforce.com', # 'https://na73.salesforce.com', TODO exist?
+            'select_fields_by_default': 'true',
+            'api_type': 'BULK',
+            'is_sandbox': 'true'
+        }
+
+    @staticmethod
+    def expected_sync_streams():
+        return {
+            'Account',
+            'Contact',
+            'Lead',
+            'Opportunity',
+            'User',
+        }
+
+    def test_run(self):
+        """
+        Verify that for each stream you can get multiple pages of data
+        when no fields are selected and only the automatic fields are replicated.
+
+        PREREQUISITE
+        For EACH stream add enough data that you surpass the limit of a single
+        fetch of data.  For instance if you have a limit of 250 records ensure
+        that 251 (or more) records have been posted for that stream.
+        """
+
+        expected_streams = self.expected_sync_streams()
+
+        # instantiate connection
+        conn_id = connections.ensure_connection(self)
+
+        # run check mode
+        found_catalogs = self.run_and_verify_check_mode(conn_id)
+
+        # table and field selection
+        test_catalogs_automatic_fields = [catalog for catalog in found_catalogs
+                                          if catalog.get('stream_name') in expected_streams]
+
+        self.perform_and_verify_table_and_field_selection(
+            conn_id, test_catalogs_automatic_fields, select_all_fields=False,
+        )
+
+        # run initial sync
+        record_count_by_stream = self.run_and_verify_sync(conn_id)
+        synced_records = runner.get_records_from_target_output()
+
+        for stream in expected_streams:
+            with self.subTest(stream=stream):
+
+                # expected values
+                expected_keys = self.expected_automatic_fields().get(stream)
+
+                # collect actual values
+                data = synced_records.get(stream)
+                record_messages_keys = [set(row['data'].keys()) for row in data['messages']
+                                        if row['action'] == 'upsert']
+
+
+                # Verify that you get some records for each stream
+                self.assertGreater(
+                    record_count_by_stream.get(stream, -1), 0,
+                    msg="The number of records is not over the stream max limit")
+
+                # Verify that only the automatic fields are sent to the target
+                for actual_keys in record_messages_keys:
+                    self.assertSetEqual(expected_keys, actual_keys)
