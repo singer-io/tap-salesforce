f89ab72a720106403918e091622719d420bccc59
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2021-09-28 14:27:17 +0200

    remove redundant requirements and add add new ones

--- setup.py
+++ setup.py
@@ -4,22 +4,21 @@ from setuptools import setup
 
 setup(
     name="tap-salesforce",
-    version="1.4.30",
+    version="1.5.0",
     description="Singer.io tap for extracting data from the Salesforce API",
     author="Stitch",
     url="https://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_salesforce"],
-    install_requires=["requests==2.22.0", "singer-python==5.8.1", "xmltodict==0.12.0"],
+    install_requires=[
+        "requests==2.22.0",
+        "singer-python==5.8.1",
+        "pydantic==1.8.2",
+        "backoff==1.8.0",
+    ],
     entry_points="""
           [console_scripts]
           tap-salesforce=tap_salesforce:main
       """,
-    packages=["tap_salesforce", "tap_salesforce.salesforce"],
-    package_data={
-        "tap_salesforce/schemas": [
-            # add schema.json filenames here
-        ]
-    },
-    include_package_data=True,
+    packages=["tap_salesforce"],
 )
