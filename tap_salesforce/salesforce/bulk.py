e236f1b00c0dabcec28f928eb6d17aaa1da18cdd
Dylan <28106103+dylan-stitch@users.noreply.github.com>
2021-10-04 13:46:58 -0400

    Fix/add sfdc state message (#120)
    * Add PK-chunk logging to capture stateMessage
    
    * Change to get(stateMessage) for safety
    
    * Update bulk.py
    
    * Break up circleci jobs
    
    * Grab the environment variables
    
    Co-authored-by: Andy Lu <andy@stitchdata.com>

--- tap_salesforce/salesforce/bulk.py
+++ tap_salesforce/salesforce/bulk.py
@@ -154,7 +154,11 @@ class Bulk():
         batch_status['job_id'] = job_id
 
         if batch_status['failed']:
-            raise TapSalesforceException("One or more batches failed during PK chunked job")
+            raise TapSalesforceException(
+                "One or more batches failed during PK chunked job. {} failed out of {} total batches. First 20 failed batches: {}".format(
+                    len(batch_status['failed']),
+                    len(batch_status['completed']) + len(batch_status['failed']),
+                    list(batch_status['failed'].items())[:20]))
 
         # Close the job after all the batches are complete
         self._close_job(job_id)
@@ -216,7 +220,7 @@ class Bulk():
 
             if not queued_batches and not in_progress_batches:
                 completed_batches = [b['id'] for b in batches if b['state'] == "Completed"]
-                failed_batches = [b['id'] for b in batches if b['state'] == "Failed"]
+                failed_batches = {b['id']: b.get('stateMessage') for b in batches if b['state'] == "Failed"}
                 return {'completed': completed_batches, 'failed': failed_batches}
             else:
                 time.sleep(PK_CHUNKED_BATCH_STATUS_POLLING_SLEEP)
