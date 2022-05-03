95e424431c60dcdd197926c583fd82cd131911ed
Karan Panchal <karan.panchal@crestdatasys.com>
2022-05-03 12:31:41 +0530

    add detialed comments

--- tests/unittests/test_salesforce_null_bookmark.py
+++ tests/unittests/test_salesforce_null_bookmark.py
@@ -2,7 +2,6 @@ import logging
 from typing import Counter
 import unittest
 from unittest import mock
-# from tap_salesforce import main
 import tap_salesforce
 from tap_salesforce import Salesforce, metrics
 from tap_salesforce.sync import sync_records
@@ -14,7 +13,8 @@ class TestNullBookmarkTesting(unittest.TestCase):
     @mock.patch('tap_salesforce.salesforce.Salesforce.query', side_effect=lambda test1, test2: [])
     def test_not_null_bookmark_for_incremental_stream(self, mocked_query):
         """
-        To ensure that after resolving writebook mark logic not get "Null" key in state
+        To ensure that after resolving write bookmark logic not get "Null" as replication key in state file, 
+        When we have selected incremental stream as Full table stream
 
         """
         sf = Salesforce(
