e83dfd6c5b5e026d396994de2b835dc5c772a809
harshpatel4crest <harsh.patel4@crestdatasys.com>
2022-05-09 18:20:54 +0530

    resolved review comments

--- tests/test_salesforce_lookback_window.py
+++ tests/test_salesforce_lookback_window.py
@@ -5,34 +5,22 @@ from base import SalesforceBaseTest
 class SalesforceLookbackWindow(SalesforceBaseTest):
 
     # subtract the desired amount of seconds form the date and return
-    def timedelta_formatted(self, dtime, seconds=0):
-        try:
-            date_stripped = datetime.strptime(dtime, self.START_DATE_FORMAT)
-            return_date = date_stripped + timedelta(seconds=seconds)
+    def timedelta_formatted(self, dtime, format, seconds=0):
+        date_stripped = datetime.strptime(dtime, format)
+        return_date = date_stripped + timedelta(seconds=seconds)
 
-            return datetime.strftime(return_date, self.START_DATE_FORMAT)
-
-        except ValueError:
-            try:
-                date_stripped = datetime.strptime(dtime, self.BOOKMARK_COMPARISON_FORMAT)
-                return_date = date_stripped + timedelta(seconds=seconds)
-
-                return datetime.strftime(return_date, self.BOOKMARK_COMPARISON_FORMAT)
-
-            except ValueError:
-                return Exception("Datetime object is not of the format: {}".format(self.START_DATE_FORMAT))
+        return datetime.strftime(return_date, format)
 
     @staticmethod
     def name():
         return 'tap_tester_salesforce_lookback_window'
 
-    @staticmethod
-    def get_properties():  # pylint: disable=arguments-differ
+    def get_properties(self):  # pylint: disable=arguments-differ
         return {
             'start_date' : '2021-11-10T00:00:00Z',
             'instance_url': 'https://singer2-dev-ed.my.salesforce.com',
             'select_fields_by_default': 'true',
-            'api_type': 'REST',
+            'api_type': self.salesforce_api,
             'is_sandbox': 'false',
             'lookback_window': 86400
         }
@@ -42,7 +30,7 @@ class SalesforceLookbackWindow(SalesforceBaseTest):
             'Account'
         }
 
-    def test_run(self):
+    def run_test(self):
         # create connection
         conn_id = connections.ensure_connection(self)
         # create state file
@@ -76,10 +64,10 @@ class SalesforceLookbackWindow(SalesforceBaseTest):
         # get replication keys
         expected_replication_keys = self.expected_replication_keys()
 
-        # get start date ie. date from which the sync started
-        start_date = state.get('bookmarks').get('Account').get('SystemModstamp')
-        # calculate the simulated start date by subtracting lookback window seconds
-        start_date_with_lookback_window = self.timedelta_formatted(start_date, seconds=-self.get_properties()['lookback_window'])
+        # get bookmark ie. date from which the sync started
+        bookmark = state.get('bookmarks').get('Account').get('SystemModstamp')
+        # calculate the simulated bookmark by subtracting lookback window seconds
+        bookmark_with_lookback_window = self.timedelta_formatted(bookmark, format=self.BOOKMARK_COMPARISON_FORMAT, seconds=-self.get_properties()['lookback_window'])
 
         for stream in expected_streams:
             with self.subTest(stream=stream):
@@ -91,18 +79,27 @@ class SalesforceLookbackWindow(SalesforceBaseTest):
                 records = [record.get('data') for record in sync_records.get(stream).get('messages')
                            if record.get('action') == 'upsert']
 
-                # check for the record if it is between lookback date and start date
+                # check for the record if it is between lookback date and bookmark
                 is_between = False
 
                 for record in records:
                     replication_key_value = record.get(replication_key)
 
-                    # Verify the sync records respect the (simulated) start date value
-                    self.assertGreaterEqual(self.parse_date(replication_key_value), self.parse_date(start_date_with_lookback_window),
+                    # Verify the sync records respect the (simulated) bookmark value
+                    self.assertGreaterEqual(self.parse_date(replication_key_value), self.parse_date(bookmark_with_lookback_window),
                                             msg='The record does not respect the lookback window.')
 
-                    # verify if the record's bookmark value is between start date and (simulated) start date value
-                    if self.parse_date(start_date_with_lookback_window) <= self.parse_date(replication_key_value) < self.parse_date(start_date):
+                    # verify if the record's bookmark value is between bookmark and (simulated) bookmark value
+                    if self.parse_date(bookmark_with_lookback_window) <= self.parse_date(replication_key_value) < self.parse_date(bookmark):
                         is_between = True
 
-                    self.assertTrue(is_between, msg='No record found between start date and lookback date.')
+                    self.assertTrue(is_between, msg='No record found between bookmark and lookback date.')
+
+    def test_run(self):
+        # run with REST API
+        self.salesforce_api = 'REST'
+        self.run_test()
+
+        # run with BULK API
+        self.salesforce_api = 'BULK'
+        self.run_test()
