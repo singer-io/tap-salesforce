4d0669875c901957d1cc56121ace76f32f717161
harshpatel4crest <harsh.patel4@crestdatasys.com>
2022-05-13 11:25:52 +0530

    updated unittest

--- tests/unittests/test_salesforce_null_bookmark.py
+++ tests/unittests/test_salesforce_null_bookmark.py
@@ -1,11 +1,8 @@
 import unittest
 from unittest import mock
-import tap_salesforce
 from tap_salesforce import Salesforce, metrics
 from tap_salesforce.sync import sync_records
 import json
-
-
 
 class TestNullBookmarkTesting(unittest.TestCase):
     @mock.patch('tap_salesforce.salesforce.Salesforce.query', side_effect=lambda test1, test2: [])
@@ -15,19 +12,9 @@ class TestNullBookmarkTesting(unittest.TestCase):
         When we have selected incremental stream as Full table stream
 
         """
-        sf = Salesforce(
-            refresh_token="test",
-            sf_client_id="test",
-            sf_client_secret="test",
-            quota_percent_total=None,
-            quota_percent_per_run=None,
-            is_sandbox=None,
-            select_fields_by_default= False,
-            default_start_date='2019-02-04T12:15:00Z',
-            api_type="BULK")
-        
+        sf = Salesforce(default_start_date='2019-02-04T12:15:00Z', api_type="BULK")
+
         sf.pk_chunking = True
-        sf.instance_url = "https://cds-e-dev-ed.my.salesforce.com"
         catalog_entry = {"stream": "OpportunityLineItem", "schema": {}, "metadata":[], "tap_stream_id": "OpportunityLineItem"}
         state = {}
         counter = metrics.record_counter('OpportunityLineItem')
