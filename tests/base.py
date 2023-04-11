489ef67e8795df9444ee51994ea8fd5bd50c85ce
btowles <btowles@stitchdata.com>
2023-04-11 15:51:24 +0000

    New fields found in discovery April 2023

--- tests/base.py
+++ tests/base.py
@@ -787,6 +787,8 @@ class SalesforceBaseTest(BaseCase):
             'CartRelatedItem': default,  # removed # 6/13/2022
             'CartTax': default,  # removed # 6/13/2022
             'CartValidationOutput': default,  # removed # 6/13/2022
+            'Conversation': incremental_last_modified,  # added # 4/11/2023
+            'ConversationParticipant': incremental_last_modified,  # added # 4/11/2023
             'Coupon': default,  # added # 10/18/2022
             'CouponHistory': incremental_created_date,  # added # 10/18/2022
             'CouponShare': incremental_last_modified, # added # 10/18/2022
