410d9723c0c88fb1a9e338e40bf62f182d3bc956
Bhuvana Venkataraman <bvenkataraman@talend.com>
2023-07-21 15:02:26 +0000

    Added comments for future work

--- tests/test_salesforce_all_fields.py
+++ tests/test_salesforce_all_fields.py
@@ -7,7 +7,12 @@ from sfbase import SFBaseTest
 
 
 class SFAllFieldsTest(AllFieldsTest, SFBaseTest):
-    """Test that with no fields selected for a stream automatic fields and custom fields are still replicated"""
+    """Test that with no fields selected for a stream automatic fields and custom fields are still replicated
+    TODO
+    This test coveres only BULK api and just couple of streams. We have separate cards to cover these cases
+    TDL-23653: [tap-salesforce]: QA - Add all the streams for the all_fields test
+    TDL-23654: [tap-salesforce]: QA - Add all-fields testcase for REST API streams
+    """
 
     salesforce_api = 'BULK'
 
