a24377d54f8560f4497f604cdccda3234d2dd0f6
Bhuvana Venkataraman <bvenkataraman@talend.com>
2023-08-15 00:05:17 +0000

    Removed get_properties and modified to have all the other functions within the tests

--- tests/test_salesforce_api_per_run_quota.py
+++ tests/test_salesforce_api_per_run_quota.py
@@ -1,53 +1,69 @@
-"""
-Test that with only custom fields selected for a stream automatic fields and custom fields  are still replicated
-"""
-import copy
-from tap_tester.base_suite_tests.api_quota_test import APIQuotaTest
 from sfbase import SFBaseTest
-from tap_tester import runner, menagerie
+from tap_tester import connections, runner, menagerie
+from tap_tester.logger import LOGGER
 
 
-class SFAPIQuota(APIQuotaTest, SFBaseTest):
-    """Test that with only custom fields selected for a stream automatic fields and custom fields are still replicated
-    TODO
-    This test coveres only BULK api and just couple of streams. We have separate cards to cover these cases
-    TDL-23653: [tap-salesforce]: QA - Add all the streams for the all_fields test
-    TDL-23654: [tap-salesforce]: QA - Add all-fields testcase for REST API streams
+class SFAPIQuota(SFBaseTest):
+    """
+    https://jira.talendforge.org/browse/TDL-23431
+    This testcase makes sure we are able to configure the tap with specific 
+    per_run quota for apis and verifies if tap is working as per the 
+    api quota limit set.
     """
 
-    salesforce_api = 'BULK'
-    expected_error = "Terminating replication due to allotted quota of 1.0% per replication"
+    """
+    Set a start date well past so that we have enough data to sync to hit the limit
+    set a low quota to make the sync fail
+    some streams are excluded as they don't have any data 
+    """
+
+    start_date = '2000-11-23T00:00:00Z'
+    per_run_quota = '1'
+    streams_to_exclude = {
+             'DatacloudAddress',
+             'DatacloudCompany',
+             'DatacloudContact',
+             'DatacloudDandBCompany',
+             'DatacloudOwnedEntity',
+             'DatacloudPurchaseUsage',
+             'FieldSecurityClassification',
+             'ServiceAppointmentStatus',
+             'WorkOrderLineItemStatus',
+             'WorkOrderStatus',
+             'ShiftStatus',
+             'WorkStepStatus',
+        }
 
     @staticmethod
     def name():
         return "tt_sf_api_quota"
 
-    def get_properties(self, original: bool = True):
-        """Configuration properties required for the tap."""
-        return_value = {
-            'start_date': '2000-11-23T00:00:00Z',
-            'instance_url': 'https://singer2-dev-ed.my.salesforce.com',
-            'select_fields_by_default': 'true',
-            'quota_percent_total': '85',
-            'quota_percent_per_run' : '1',
-            'api_type': self.salesforce_api,
-            'is_sandbox': 'false'
-        }
-        if original:
-            return return_value
-
-        # This test needs the new connections start date to be larger than the default
-        assert self.start_date > return_value["start_date"]
-
-        return_value["start_date"] = self.start_date
-        return return_value
-
     def streams_to_test(self):
         return self.expected_streams().difference( self.streams_to_exclude )
 
-    def test_all_streams_sync_records(self):
-        for stream in self.streams_to_test():
-            with self.subTest(stream=stream):
+    @staticmethod
+    def streams_to_selected_fields():
+        # Select all fields in the selected catalogs (streams)
+        return {}
 
-                # gather results
-                record_count = self.record_count_by_stream
+    def test_api_per_run_quota(self):
+        """
+        Run the tap in check mode and verify it returns the error for quota limit reached. If not, proceed to sync mode.
+        """
+        expected_per_run_error = "Terminating replication due to allotted quota"
+        conn_id = connections.ensure_connection(self)
+
+        found_catalogs = self.run_and_verify_check_mode( conn_id)
+
+        # table and field selection
+        test_catalogs = [catalog for catalog in found_catalogs
+                           if catalog.get('stream_name') in self.streams_to_test()]
+
+        # non_selected_fields are none
+        self.perform_and_verify_table_and_field_selection(conn_id, test_catalogs)
+
+        with self.assertRaises(Exception) as ex:
+             record_count_by_stream = self.run_and_verify_sync_mode(conn_id)
+
+        self.assertIn(expected_per_run_error, str(ex.exception) )
+
--- tests/test_salesforce_api_total_quota.py
+++ tests/test_salesforce_api_total_quota.py
@@ -1,50 +1,57 @@
-"""
-Test that with only custom fields selected for a stream automatic fields and custom fields  are still replicated
-"""
-import copy
-from tap_tester.base_suite_tests.api_quota_test import APIQuotaTest
 from sfbase import SFBaseTest
-from tap_tester import runner, menagerie
+from tap_tester import runner, menagerie, LOGGER, connections
 
 
-class SFAPIQuota(APIQuotaTest, SFBaseTest):
-    """Test that with only custom fields selected for a stream automatic fields and custom fields are still replicated
-    TODO
-    This test coveres only BULK api and just couple of streams. We have separate cards to cover these cases
-    TDL-23653: [tap-salesforce]: QA - Add all the streams for the all_fields test
-    TDL-23654: [tap-salesforce]: QA - Add all-fields testcase for REST API streams
+class SFAPIQuota( SFBaseTest):
+    """
+    https://jira.talendforge.org/browse/TDL-23431
+    This testcase makes sure we are able to configure the tap with specific
+    total quota for apis and verifies if tap is working as per the
+    api quota limit set.
     """
 
-    salesforce_api = 'BULK'
-    expected_error = "Terminating replication to not continue past configured percentage of 2.0% total quota"
+    """
+    Set a start date well past so that we have enough data to sync to hit the limit
+    set a low quota to make the sync fail
+    some streams are excluded as they don't have any data
+    """
+
+    start_date = '2000-11-23T00:00:00Z'
+    total_quota = '1'
+    streams_to_exclude = {
+             'DatacloudAddress',
+             'DatacloudCompany',
+             'DatacloudContact',
+             'DatacloudDandBCompany',
+             'DatacloudOwnedEntity',
+             'DatacloudPurchaseUsage',
+             'FieldSecurityClassification',
+             'ServiceAppointmentStatus',
+             'WorkOrderLineItemStatus',
+             'WorkOrderStatus',
+             'ShiftStatus',
+             'WorkStepStatus',
+        }
 
     @staticmethod
     def name():
         return "tt_sf_api_quota_total"
 
-    def get_properties(self, original: bool = True):
-        """Configuration properties required for the tap."""
-        return_value = {
-            'start_date': '2000-11-23T00:00:00Z',
-            'instance_url': 'https://singer2-dev-ed.my.salesforce.com',
-            'select_fields_by_default': 'true',
-            'quota_percent_total': '2',
-            'quota_percent_per_run' : '1',
-            'api_type': self.salesforce_api,
-            'is_sandbox': 'false'
-        }
-        if original:
-            return return_value
+    def streams_to_test(self):
+        return self.expected_streams().difference( self.streams_to_exclude )
 
-        # This test needs the new connections start date to be larger than the default
-        assert self.start_date > return_value["start_date"]
+    def test_api_total_quota(self):
+        """
+        Run the tap in check mode and verify it returns the error for quota limit reached. 
+        """
+        expected_total_quota_error = "Terminating replication to not continue past configured percentage of 1.0% total quota"
 
-        return_value["start_date"] = self.start_date
-        return return_value
+        conn_id = connections.ensure_connection(self)
 
-    def test_all_streams_sync_records(self):
-        for stream in self.streams_to_test():
-            with self.subTest(stream=stream):
+        # run check mode
+        # Run a check job using orchestrator (discovery)
+        with self.assertRaises(Exception) as ex:
+             check_job_name = self.run_and_verify_check_mode( conn_id)
 
-                # gather results
-                record_count = self.record_count_by_stream
+        self.assertIn(expected_total_quota_error, str(ex.exception) )
+
