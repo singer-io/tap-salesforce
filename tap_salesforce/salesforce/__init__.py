c44901f5a9a6527ce86c3417512fede4f5bf03f3
prijendev <prijen.khokhani@qlik.com>
2024-10-03 09:35:13 +0530

    TDL-26523: Skip few more streams for Bulk API

--- tap_salesforce/salesforce/__init__.py
+++ tap_salesforce/salesforce/__init__.py
@@ -81,15 +81,9 @@ UNSUPPORTED_BULK_API_SALESFORCE_OBJECTS = set(['TaskPriority',
                                                'WorkOrderLineItemStatus',
                                                'ServiceAppointmentStatus',
                                                'ContractStatus',
-                                               'PartnerRole'])
+                                               'PartnerRole',
+                                               'TaskStatus'])
 
-# UNSUPPORTED_BULK_API_SALESFORCE_OBJECTS = set(['AssetTokenEvent',
-#                                                'AttachedContentNote',
-#                                                'EventWhoRelation',
-#                                                'QuoteTemplateRichTextData',
-#                                                'TaskWhoRelation',
-#                                                'SolutionStatus',
-#                                                'TaskStatus',])
 
 # The following objects have certain WHERE clause restrictions so we exclude them.
 QUERY_RESTRICTED_SALESFORCE_OBJECTS = set(['Announcement',
