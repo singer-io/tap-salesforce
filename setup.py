3c629c7557930e00a2fe88b506125fd436051c14
prijendev <prijen.khokhani@qlik.com>
2024-10-10 19:28:29 +0530

    Update changelog md

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-salesforce',
-      version='2.0.1',
+      version='2.0.2',
       description='Singer.io tap for extracting data from the Salesforce API',
       author='Stitch',
       url='https://singer.io',
