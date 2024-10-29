2d148024f81b93bde68a254f1dfd9ce57de0b25f
prijendev <prijen.khokhani@qlik.com>
2024-10-29 13:38:27 +0530

    Update release minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-salesforce',
-      version='2.0.2',
+      version='2.1.0',
       description='Singer.io tap for extracting data from the Salesforce API',
       author='Stitch',
       url='https://singer.io',
